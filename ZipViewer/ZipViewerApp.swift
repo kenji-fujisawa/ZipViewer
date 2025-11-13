1724e66a7b418ee31fc45dde135b739302cc27e6
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2025-11-13 14:59:43 +0900

    initial commit

---
+++ ZipViewer/ZipViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipViewerApp.swift
+//  ZipViewer
+//
+//  Created by uhimania on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct ZipViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZipViewerTests/ZipViewerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ZipViewerTests.swift
+//  ZipViewerTests
+//
+//  Created by uhimania on 2025/11/13.
+//
+
+import Testing
+
+struct ZipViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
