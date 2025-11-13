183b0f35f6934129c4e97f5d394316b492bb1d5a
kenji-fujisawa <uhimania@uhimanianoMacBook-Air.local>
2025-11-13 19:32:53 +0900

    zip ファイル内の画像閲覧機能実装

--- ZipViewer/ContentView.swift
+++ ZipViewer/ContentView.swift
@@ -6,16 +6,128 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
+import ZIPFoundation
+
+struct ZipItem: Identifiable, Hashable {
+    let id = UUID()
+    var archive: Archive
+    var entry: Entry
+    
+    var filename: String {
+        String(entry.path.split(separator: "/").last ?? "")
+    }
+    
+    var image: NSImage? {
+        var _data = Data()
+        do {
+            let _ = try archive.extract(entry) { data in
+                _data.append(data)
+            }
+        } catch {
+            print(error)
+        }
+        return NSImage(data: _data)
+    }
+    
+    static func == (lhs: ZipItem, rhs: ZipItem) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
 
 struct ContentView: View {
+    @Environment(AppDelegate.self) private var appDelegate
+    @State private var showImporter: Bool = false
+    @State private var items: [ZipItem] = []
+    @State private var selected: ZipItem? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationSplitView {
+            SidebarView(items: $items, selected: $selected)
+        } detail: {
+            DetailView(item: $selected)
         }
         .padding()
+        .focusedSceneValue(\.openFileAction, OpenFileAction(showImporter: { showImporter = true }))
+        .fileImporter(isPresented: $showImporter, allowedContentTypes: [.zip], allowsMultipleSelection: false) { result in
+            switch result {
+            case .success(let urls):
+                if let url = urls.first {
+                    guard url.startAccessingSecurityScopedResource() else { return }
+                    defer { url.stopAccessingSecurityScopedResource() }
+                    loadEntries(url: url)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        .dropDestination(for: URL.self) { items, session in
+            if let url = items.first {
+                loadEntries(url: url)
+            }
+        }
+        .onChange(of: appDelegate.urls) { _, _ in
+            if let url = appDelegate.urls.first {
+                loadEntries(url: url)
+            }
+        }
+    }
+    
+    private func loadEntries(url: URL) {
+        items.removeAll()
+        
+        do {
+            let archive = try Archive(url: url, accessMode: .read)
+            for entry in archive {
+                if entry.path.last != "/" {
+                    items.append(ZipItem(archive: archive, entry: entry))
+                }
+            }
+        } catch {
+            print(error)
+        }
+        
+        if let item = items.first {
+            selected = item
+        }
+    }
+    
+    private struct SidebarView: View {
+        @Binding var items: [ZipItem]
+        @Binding var selected: ZipItem?
+        
+        var body: some View {
+            List(items, selection: $selected) { item in
+                NavigationLink(value: item) {
+                    VStack {
+                        if let img = item.image {
+                            Image(nsImage: img)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                        }
+                        Text(item.filename)
+                    }
+                }
+            }
+        }
+    }
+    
+    private struct DetailView: View {
+        @Binding var item: ZipItem?
+        
+        var body: some View {
+            if let item = item,
+               let img = item.image {
+                ScrollView {
+                    Image(nsImage: img)
+                }
+            }
+        }
     }
 }
 
--- ZipViewer/ZipViewerApp.swift
+++ ZipViewer/ZipViewerApp.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 @main
 struct ZipViewerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(appDelegate)
         }
+        .commands {
+            CommandGroup(replacing: .newItem) {}
+            CommandGroup(replacing: .pasteboard) {}
+            CommandGroup(replacing: .undoRedo) {}
+            OpenFileCommands()
+        }
+    }
+}
+
+@Observable
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var urls: [URL] = []
+    
+    func application(_ application: NSApplication, open urls: [URL]) {
+        self.urls = urls
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
 }
